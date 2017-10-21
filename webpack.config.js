6abe49e6b4e733993f7abbdf9a74b6e1a206d5ad
Lee Fernandes <leefernandes@gmail.com>
2017-10-21 10:12:40 -0400

    gitignore update

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        include: ["src"],
+        include: /(src)/,
         use: {
           loader: 'babel-loader',
           options: pkg.babel,
