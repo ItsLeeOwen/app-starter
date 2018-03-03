d0f74ed26a2f301c77a05fdaef5d1eebb4d52f47
Lee Fernandes <itsleeowen@gmail.com>
2018-03-02 20:45:34 -0500

    react support

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import React from "react"
+
+import {
+  Link,
+  Route,
+} from 'react-router-dom'
+
+import Fluffykins from "./fluffykins"
+import Abbykins from "./abbykins"
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          <Link to="/">React App Starter</Link>
+        </h1>
+        <nav>
+          <Link to="/abbykins">Abbykins</Link>
+          <br />
+          <Link to="/fluffykins">Fluffykins</Link>
+        </nav>
+
+        <Route
+          path="/abbykins"
+          component={Abbykins} />
+        <Route
+          path="/fluffykins"
+          component={Fluffykins} />
+      </div>
+    )
+  }
+}
