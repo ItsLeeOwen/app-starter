67d0977ec20fcf6b91649aef5491ca3cf9d93e28
Lee Fernandes <itsleeowen@gmail.com>
2019-01-20 22:47:06 -0500

    vanilla js/html/scss

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-import './index.scss'
+import "./index.scss"
 
-import React from "react"
-import ReactDOM from "react-dom"
-import { HashRouter as Router, Route } from 'react-router-dom'
-
-import App from "./app"
-
-ReactDOM.render((
-  <Router>
-    <App />
-  </Router>
-), document.querySelector("main"))
+console.log("index.js")
