dfbb7bc2c33d497be4fdddf908c539f1d77eaef9
Lee Fernandes <itsleeowen@gmail.com>
2019-02-24 10:22:48 -0500

    vue components

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-// import Vue from "vue"
-// import jsx from "./component/jsx-component"
-// import sfc from "./component/single-file-component"
-// import slc from "./component/string-literal-component"
-import "./carousel"
+import Vue from "vue"
+import app from "./app"
+import jsx from "./component/jsx-component"
+import sfc from "./component/single-file-component"
+import slc from "./component/string-literal-component"
 import "./index.scss"
 
 // string literal component
-// Vue.component("my-string-literal-component", slc)
+Vue.component("my-string-literal-component", slc)
 
 // single file component example
-// Vue.component("my-single-file-component", sfc)
+Vue.component("my-single-file-component", sfc)
 
 // jsx based vue component
-// Vue.component("my-jsx-component", jsx)
+Vue.component("my-jsx-component", jsx)
 
 // initialize vue app
-// new Vue(app)
+new Vue(app)
