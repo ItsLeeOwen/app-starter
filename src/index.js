0fa0e114ba38ca5975489dda1982dd1e75754618
Lee Fernandes <itsleeowen@gmail.com>
2019-02-24 17:01:23 -0500

    cleanup

--- src/index.js
+++ src/index.js
@@ -20,37 +20,3 @@ Vue.component("carousel", carousel)
 
 // initialize vue app
 new Vue(app)
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   const carousel = document.querySelector(".my-carousel")
-
-//   const content = [
-//     { image: "asset/poster/terminator.jpg", data: "terminator" },
-//     { image: "asset/poster/pulpfiction.jpg", data: "pulpfiction" },
-//     { image: "asset/poster/scarface.jpg", data: "scarface" },
-//     { image: "asset/poster/ghostbusters.jpg", data: "ghostbusters" },
-//     { image: "asset/poster/goonies.jpg", data: "goonies" },
-//     { image: "asset/poster/terminator.jpg", data: "terminator" },
-//     { image: "asset/poster/pulpfiction.jpg", data: "pulpfiction" },
-//     { image: "asset/poster/scarface.jpg", data: "scarface" },
-//     { image: "asset/poster/ghostbusters.jpg", data: "ghostbusters" },
-//     { image: "asset/poster/goonies.jpg", data: "goonies" },
-//   ].map(item => {
-//     const element = document.createElement("div")
-//     element.style.backgroundImage = `url("${item.image}")`
-//     return {
-//       element,
-//       data: item.data,
-//     }
-//   })
-
-//   new Carousel({
-//     carousel,
-//     content,
-//     contentHeight: 550,
-//     contentWidth: 365,
-//     onSelect: selected => {
-//       console.log("onSelect", selected)
-//     },
-//   })
-// })
